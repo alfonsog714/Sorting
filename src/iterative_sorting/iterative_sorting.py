8dbc872a8b1b47695773e4606ea9712d534fe90f
Alfonso <alfonso.garcia2324@gmail.com>
2019-09-09 14:05:51 -0700

    finally got a version of selection sorting working, i definitely need to study more

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,24 +1,20 @@
 import random
 # TO-DO: Complete the selection_sort() function below 
+
 def selection_sort( arr ):
     # loop through n-1 elements
     for i in range(0, len(arr) - 1): # 0, 7
-        cur_index = i 
-        smallest_index = cur_index
-        
+        smallest_index = i
+        for j in range(smallest_index + 1, len(arr)):
         # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
         # a. Loop through elements on right-hand-side of current index and find the smallest element
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         #b. Swap the element at current index with the smallest element found in above loop
-
-
-
-
         # TO-DO: swap
-
-
-
+        if smallest_index != i:
+            arr[i], arr[smallest_index] = arr[smallest_index], arr[i]
 
     return arr
 
@@ -36,24 +32,29 @@ def count_sort( arr, maximum=-1 ):
 
 
 
-a = [1,2,3,4,5,6,7,8]
-random.shuffle(a)
-for i in range(0, len(a) - 1): 
-    """ 
-    realization - by using the range function, you're providing the for loop the array of indices, e.g [0, 1, 2, 3, 4, 5, 6]
-    You cut off the last number by making it the length of the array - 1. Before this, I thought the i was the value in the array
-    """
-    minIndex = i
-    print(f"This is minIndex before entering the second loop: {minIndex}")
-    print(f"This is I, AKA the current index in the loop: {i}")
-    for j in range(i + 1, len(a)):
-        """
-        Tried using range(a[i:]), didn't work.
-        """
-        print(f"This is J, AKA the index of the next number in the array to the right of I: {j}")
-        if a[j] < a[minIndex]:
-            minIndex = j
-            print(f"This is minIndex after running through the if clause: {minIndex}")
+# a = [1,2,3,4,5,6,7,8, 9, 10, 11, 12, 13,14,15]
+# random.shuffle(a)
+# for i in range(0, len(a) - 1): 
+#     """ 
+#     realization - by using the range function, you're providing the for loop the array of indices, e.g [0, 1, 2, 3, 4, 5, 6]
+#     You cut off the last number by making it the length of the array - 1. Before this, I thought the i was the value in the array
+#     """
+#     minIndex = i
+#     print(f"This is minIndex before entering the second loop: {minIndex}")
+#     print(f"This is I, AKA the current index in the loop: {i}")
+#     for j in range(i + 1, len(a)):
+#         """
+#         Tried using range(a[i:]), didn't work.
+#         """
+#         print(f"This is J, AKA the index of the next number in the array to the right of I: {j}")
+#         if a[j] < a[minIndex]:
+#             minIndex = j
+#             print(f"This is minIndex after running through the if clause: {minIndex}")
+    
+#     if minIndex != i:
+#         a[i], a[minIndex] = a[minIndex], a[i]
+
+# print(a)
 
 # random.shuffle(a)
 # # i = 1
