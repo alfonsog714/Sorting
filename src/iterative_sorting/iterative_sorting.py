01b9cf4584b357ab40143861be294684e637ffd0
Alfonso <alfonso.garcia2324@gmail.com>
2019-09-09 12:15:42 -0700

    printing out values so i can understand what's going on better, starting to realize things

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -2,14 +2,14 @@ import random
 # TO-DO: Complete the selection_sort() function below 
 def selection_sort( arr ):
     # loop through n-1 elements
-    for i in range(len(arr)):
+    for i in range(0, len(arr) - 1): # 0, 7
         cur_index = i 
         smallest_index = cur_index
-        print(cur_index)
-        print(smallest_index)
+        
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc) 
         # a. Loop through elements on right-hand-side of current index and find the smallest element
+
         #b. Swap the element at current index with the smallest element found in above loop
 
 
@@ -38,7 +38,22 @@ def count_sort( arr, maximum=-1 ):
 
 a = [1,2,3,4,5,6,7,8]
 random.shuffle(a)
-# i = 1
-# print(a[i + 1:])
+for i in range(0, len(a) - 1): 
+    """ 
+    realization - by using the range function, you're providing the for loop the array of indices, e.g [0, 1, 2, 3, 4, 5, 6]
+    You cut off the last number by making it the length of the array - 1. Before this, I thought the i was the value in the array
+    """
+    minIndex = i
+    # print(i)
+    for j in range(i + 1, len(a)):
+        print(f"This is I, AKA the current index in the loop: {i}")
+        """
+        Tried using range(a[i:]), didn't work.
+        """
+        print(f"This is J, AKA the current number(?) in the numbers to the right of I: {j}")
 
-print(selection_sort(a))
+# random.shuffle(a)
+# # i = 1
+# # print(a[i + 1:])
+
+# print(selection_sort(a))
